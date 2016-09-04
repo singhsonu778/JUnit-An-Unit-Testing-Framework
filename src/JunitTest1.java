5501d7ff3940a3813596f1cb9692edd276127169
singhsonu778 <singhsonu778@gmail.com>
2016-09-04 23:12:12 +0530

    Use Annotations to run certain methods before/after class and each test

--- src/JunitTest1.java
+++ src/JunitTest1.java
@@ -1,7 +1,42 @@
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class JunitTest1 {
+   
+   @BeforeClass
+   public static void beforeClass() {
+      System.out.println("before class");
+   }
+
+   @AfterClass
+   public static void afterClass() {
+      System.out.println("after class");
+   }
+
+   @Before
+   public void beforeTest() {
+      System.out.println("before test");
+   }
+	
+   @After
+   public void afterTest() {
+      System.out.println("after test");
+   }
+   
+   @Test
+   public void runTest() {
+      System.out.println("test to run");
+   }
+	
+   @Ignore
+   public void ignoreTest() {
+      System.out.println("test to ignore");
+   }
    
    @Test
    public void testInteger() {
