e09559b21666e998e31abd40a9350a214a5d139f
Sonu Kumar Singh <singhsonu778@gmail.com>
2016-09-08 23:53:36 +0530

    Use @InjectMocks annotations

--- src/RandomNumberListSorterTest.java
+++ src/RandomNumberListSorterTest.java
@@ -5,20 +5,17 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class RandomNumberListSorterTest {
+	@InjectMocks
 	RandomNumberListSorter listSorter;
 	@Mock
 	RandomNumberListGenerator listGenerator;
-
-	@Before
-	public void setUp() {
-		listSorter = new RandomNumberListSorter(listGenerator);
-	}
 
 	@Test
 	public void shouldGetSortedRandomNumberList() {
