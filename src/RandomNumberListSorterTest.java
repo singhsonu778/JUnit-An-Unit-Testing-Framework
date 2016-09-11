d2e700e7e56688715f1bcaed17b6f26096f30537
singhsonu778 <singhsonu778@gmail.com>
2016-09-11 17:40:29 +0530

    Use ArgumentCaptor to test the arguments of method calls

--- src/RandomNumberListSorterTest.java
+++ src/RandomNumberListSorterTest.java
@@ -5,6 +5,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -21,6 +22,7 @@ public class RandomNumberListSorterTest {
 	public void shouldGetSortedRandomNumberList() {
 		List<Integer> fakeList = Arrays.asList(15, 20, 3, 16, 5);
 		Mockito.when(listGenerator.getList(5)).thenReturn(fakeList);
+		ArgumentCaptor<Integer> argumentCaptor = ArgumentCaptor.forClass(Integer.class);
 
 		List<Integer> list = listSorter.getSortedRandomNumberList(5);
 
@@ -29,7 +31,8 @@ public class RandomNumberListSorterTest {
 		Assert.assertEquals(list.get(2), new Integer(15));
 		Assert.assertEquals(list.get(3), new Integer(16));
 		Assert.assertEquals(list.get(4), new Integer(20));
-		Mockito.verify(listGenerator, Mockito.times(1)).getList(5);
+		Mockito.verify(listGenerator, Mockito.times(1)).getList(argumentCaptor.capture());
+		Assert.assertEquals(new Integer(5), argumentCaptor.getValue());
 
 		list.forEach(System.out::println);
 	}
